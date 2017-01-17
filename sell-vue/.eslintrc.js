613f68a0dace9056f6804ff678e9c52dc70b9b4a
xypisces <953945582@qq.com>
2017-01-18 00:22:13 +0800

    update vue

--- sell-vue/.eslintrc.js
+++ sell-vue/.eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
+    'semi': ['error','always'],
+    'indent':0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
