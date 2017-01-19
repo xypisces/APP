07d9dcb0fe85b81d1f63a7a46d0829e16fb5aeda
xypisces <953945582@qq.com>
2017-01-19 20:29:09 +0800

    update

--- sell-vue/src/App.vue
+++ sell-vue/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-    <div id="app">
-        <v-header></v-header>
-        <div class="tab">
-            <div class="tab-item">商品</div>
-            <div class="tab-item">评价</div>
-            <div class="tab-item">店家</div>
+    <v-header></v-header>
+    <div class="tab">
+        <div class="tab-item">
+            <a v-link="{path:'/goods'}">商品</a>
         </div>
-        <div class="content">
-            i am content
+        <div class="tab-item">
+            <a v-link="{path:'/ratings'}">评论</a>
+        </div>
+        <div class="tab-item">
+            <a v-link="{path:'/seller'}">商铺</a>
         </div>
     </div>
+    <router-view></router-view>
 </template>
 
 <script>
@@ -21,17 +23,18 @@
     }
   };
 
+
+
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  #app
     .tab
-      display: flex
-      width: 100%
-      height: 40px
-      line-height: 40px
-      .tab-item
-        flex: 1
-        text-align: center
+        display: flex
+        width: 100%
+        height: 40px
+        line-height: 40px
+        .tab-item
+            flex: 1
+            text-align: center
 
 </style>
--- sell-vue/src/main.js
+++ sell-vue/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
 import App from './App';
+import VueRouter from 'vue-router';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter);
+
+let App = Vue.extend(App);
+let router = new VueRouter();
+router.map({
+  '/goods':{
+    component:'components/goods/goods'
+  }
+
 });
+router.start(App,'#app');
+router.go('/goods');
