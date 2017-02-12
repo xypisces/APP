7898f9f016fe29024db9c0116359c1b2f5164b87
xypisces <953945582@qq.com>
2017-02-12 21:03:27 +0800

    update

--- sell-vue/src/App.vue
+++ sell-vue/src/App.vue
@@ -1,39 +1,64 @@
 <template>
-    <div>
-        <v-header></v-header>
-        <div class="tab">
-            <div class="tab-item">
-                <a v-link="{path:'/goods'}">商品</a>
-            </div>
-            <div class="tab-item">
-                <a v-link="{path:'/ratings'}">评论</a>
-            </div>
-            <div class="tab-item">
-                <a v-link="{path:'/seller'}">商店</a>
-            </div>
-        </div>
-        <router-view></router-view>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <a v-link="{path:'/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/seller'}">商店</a>
+      </div>
     </div>
+    <router-view></router-view>
+  </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   import header from './components/header/header.vue';
 
+  const ERR_OK = 0;
   export default {
+    data() {
+      return {
+        seller: {}
+      };
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+        }
+      });
+    },
     components: {
       'v-header': header
     }
   };
+
 </script>
 
+
 <style lang="stylus" rel="stylesheet/stylus">
-    .tab
-        display: flex
-        width: 100%
-        height: 40px
-        line-height: 40px
-        .tab-item
-            flex: 1
-            text-align: center
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
 
 </style>
+
--- sell-vue/src/components/header/header.vue
+++ sell-vue/src/components/header/header.vue
@@ -1,13 +1,49 @@
 <template>
-    <div class="header">
-        i am header.
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width="64" height="64" :src="seller.avatar">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div class="suppor" v-if="seller.supports">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {};
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .header
+    color: #fff
+    background: #000
+    .content-wrapper
+      padding: 24px 12px 18px 24px
+      font-size: 0
+      .avatar
+        display: inline-block
+      .content
+        display: inline-block
+        margin-left: 16px
+        font-size: 14px
+        .title
+          margin: 2px 0 8px 0
 </style>
